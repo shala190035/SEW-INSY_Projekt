d4fe9146c5a9b80ae8269340932932ad5588e9f1
shala190035 <55692386+shala190035@users.noreply.github.com>
2023-01-26 16:48:47 +0100

    reset,add,remove,history geht

--- src/index_test.php
+++ src/index_test.php
@@ -5,68 +5,103 @@ include("connection.php");
 include("functions.php");
 
 $user_data = check_login($con);
-
-if(!empty($_POST['calorie'])) {
-    $calorie = $_POST['calorie'];
-    $id = $_SESSION['id'];
-    $check_query = "SELECT * FROM day_calorie WHERE user_id = '$id'";
-    $check_result = mysqli_query($con, $check_query);
-    if(mysqli_num_rows($check_result) > 0) {
-        $query = "UPDATE day_calorie SET calorie = '$calorie' WHERE user_id = '$id'";
-        $result = mysqli_query($con, $query);
-    } else {
-        $query = "INSERT INTO day_calorie (user_id, calorie) VALUES ('$id', '$calorie')";
-        $result = mysqli_query($con, $query);
-    }
-    
-
-    if ($result === false) {
-        error_log(mysqli_error($con));
-        echo "There was an error saving the calories to the database.";
-    } else {
-        if (mysqli_affected_rows($con) == 0) {
-            error_log("The calories was not saved to the database because the query did not affect any rows.");
-            echo "calories was not saved to the database.";
-        } else {
-            error_log("calories was successfully saved to the database.");
-            echo "calories was successfully saved to the database.";
-        }
-    }
-    error_log($calorie);
-} else {
-    error_log("The calorie variable was not posted.");
-}
-
-/* if(!empty($_POST['sport'])) {
-    $sport = $_POST['sport'];
-    $id = $_SESSION['user_id'];
-    $query = "UPDATE day_calorie SET calorie = calorie + '$sport' WHERE user_id = '$id'";
-    $result = mysqli_query($con,$query);
-
-    if ($result === false) {
-        error_log(mysqli_error($con));
-        echo "There was an error adding the sport calories to the database.";
-    } else {
-        if (mysqli_affected_rows($con) == 0) {
-            error_log("The sport calories was not saved to the database because the query did not affect any rows.");
-            echo "sport calories was not saved to the database.";
-        } else {
-            error_log("sport calories was successfully added to the database.");
-            echo "sport calories was successfully added to the database.";
-        }
-    }
-    error_log($sport);
-} else {
-    error_log("The sport variable was not posted.");
-}
-*/
-
 $id = $_SESSION['id'];
 $query = "SELECT calorie FROM day_calorie WHERE user_id='$id'";
 $result = mysqli_query($con, $query);
 $query_history = "SELECT calorie, date_saved FROM saved_calorie WHERE user_id='$id'";
 $result_history = mysqli_query($con, $query_history);
+
+if(isset($_POST['calorie'])){
+    if(!empty($_POST['calorie'])) {
+        $calorie = $_POST['calorie'];
+        $id = $_SESSION['id'];
+        $check_query = "SELECT * FROM day_calorie WHERE user_id = '$id'";
+        $check_result = mysqli_query($con, $check_query);
+        if(mysqli_num_rows($check_result) > 0) {
+            $query = "UPDATE day_calorie SET calorie = '$calorie' WHERE user_id = '$id'";
+            $result = mysqli_query($con, $query);
+        } else {
+            $query = "INSERT INTO day_calorie (user_id, calorie) VALUES ('$id', '$calorie')";
+            $result = mysqli_query($con, $query);
+        }
+
+        if ($result === false) {
+            error_log(mysqli_error($con));
+            echo "There was an error saving the calories to the database.";
+        } else {
+            if (mysqli_affected_rows($con) == 0) {
+                error_log("The calories was not saved to the database because the query did not affect any rows.");
+                echo "calories was not saved to the database.";
+            } else {
+                error_log("calories was successfully saved to the database.");
+                echo "calories was successfully saved to the database.";
+            }
+        }
+        error_log($calorie);
+    } else {
+        error_log("The calorie variable was not posted.");
+    }
+}
+
+if(isset($_POST['sport'])){
+    if(!empty($_POST['sport'])) {
+        $sport = $_POST['sport'];
+        $id = $_SESSION['id'];
+        $query = "UPDATE day_calorie SET calorie = calorie + '$sport' WHERE user_id = '$id'";
+        $result = mysqli_query($con,$query);
+
+        if ($result === false) {
+            error_log(mysqli_error($con));
+            echo "There was an error adding the sport calories to the database.";
+        } else {
+            if (mysqli_affected_rows($con) == 0) {
+                error_log("The sport calories was not saved to the database because the query did not affect any rows.");
+                echo "sport calories was not saved to the database.";
+            } else {
+                error_log("sport calories was successfully added to the database.");
+                echo "sport calories was successfully added to the database.";
+            }
+        }
+        error_log($sport);
+    } else {
+        error_log("The sport variable was not posted.");
+    }
+}
+
+if(isset($_POST['food'])){
+    if(!empty($_POST['food'])) {
+        $food = $_POST['food'];
+        $id = $_SESSION['id'];
+        $query = "UPDATE day_calorie SET calorie = calorie - '$food' WHERE user_id = '$id'";
+        $result = mysqli_query($con,$query);
+
+        if ($result === false) {
+            error_log(mysqli_error($con));
+            echo "There was an error adding the food calories to the database.";
+        } else {
+            if (mysqli_affected_rows($con) == 0) {
+                error_log("The food calories was not saved to the database because the query did not affect any rows.");
+                echo "food calories was not saved to the database.";
+            } else {
+                error_log("food calories was successfully added to the database.");
+                echo "food calories was successfully added to the database.";
+            }
+        }
+        error_log($food);
+    } else {
+        error_log("The food variable was not posted.");
+    }
+}
+
+if(isset($_POST['reset'])) {
+    $id = $_SESSION['id'];
+    $query = "UPDATE day_calorie SET calorie = 0 WHERE user_id = '$id'";
+    $result = mysqli_query($con,$query);
+
+    error_log("The calorie variable was reset.");
+}
 ?>
+
 
 <html>
 <head>
@@ -78,7 +113,22 @@ $result_history = mysqli_query($con, $query_history);
         <input type="number" id="calorie-input" name="calorie">
     </div>
     <button id="send-calorie">Send calorie</button>
-    <button type="reset" id="reset-calorie">Reset</button>
+
+    <div>
+        <button id="reset">reset</button>
+    </div>
+    <div>
+        <label for="sport-input">Enter calorie sport:</label>
+        <input type="number" id="sport-input" name="sport">
+    </div>
+    <button id="send-sport">Send sport calorie</button>
+
+    <div>
+        <label for="food-input">Enter calorie food:</label>
+        <input type="number" id="food-input" name="food">
+    </div>
+    <button id="send-food">Send food calorie</button>
+
     <div>
         <h3>current Calorie intake:</h3>
         <?php
@@ -130,9 +180,61 @@ docReady(function () {
         }
         location.reload();
     });
-    $('#reset-calorie').click(function(){
-        $('#calorie-input').val('');
+
+    $('#reset').click(function() {
+        $.ajax({
+            url: 'http://localhost/fitness/index_test.php',
+            type: 'POST',
+            data: { reset: 0 },
+            success: function(response) {
+                console.log('The Ajax request was successful.');
+            },
+            error: function(error) {
+                console.log('There was an error with the Ajax request:', error);
+            }
+        });
+
+        location.reload();
     });
+
+    $('#send-sport').click(function() {
+        if($('#sport-input').val() !== ""){
+            $.ajax({
+                url: 'http://localhost/fitness/index_test.php',
+                type: 'POST',
+                data: { sport: $('#sport-input').val() },
+                success: function(response) {
+                    console.log('The Ajax request was successful.');
+                },
+                error: function(error) {
+                    console.log('There was an error with the Ajax request:', error);
+                }
+            });
+        }else{
+            alert("Please enter a value for calorie");
+        }
+        location.reload();
+    });
+
+    $('#send-food').click(function() {
+        if($('#food-input').val() !== ""){
+            $.ajax({
+                url: 'http://localhost/fitness/index_test.php',
+                type: 'POST',
+                data: { food: $('#food-input').val() },
+                success: function(response) {
+                    console.log('The Ajax request was successful.');
+                },
+                error: function(error) {
+                    console.log('There was an error with the Ajax request:', error);
+                }
+            });
+        }else{
+            alert("Please enter a value for calorie");
+        }
+        location.reload();
+    });
+
     
 });
 
